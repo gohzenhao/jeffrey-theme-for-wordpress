3976f1a059c5fa9c4bfef852e20a78948fc09f5c
gohzenhao <gohzenhao@gmail.com>
2020-06-03 16:35:42 +1000

    Further enhancements

--- includes/section-cars.php
+++ includes/section-cars.php
@@ -1,15 +1,8 @@
 <?php if( have_posts() ) : while( have_posts() ): the_post(); ?>
 
 
-<p><?php echo get_the_date(); ?></p>
-
 <?php the_content(); ?>
-<?php
-$fname = get_the_author_meta('first_name');
-$lname = get_the_author_meta('last_name');
-?>
 
--- <?php echo $fname; ?> <?php echo $lname;?>
 
 <?php
 
--- single-cars.php
+++ single-cars.php
@@ -3,12 +3,14 @@
 <section class="page-wrap">
 <div class="container">
 
+    <?php if(has_post_thumbnail()): ?>
+        <img src="<?php the_post_thumbnail_url();?>" alt="<?php the_title();?>"/>
+    <?php endif;?>
+
     <h1><?php the_title();?></h1>
 
     <?php get_template_part('includes/section', 'cars'); ?>
-    <?php get_template_part('includes/section', 'content'); ?>
-
-    <?php get_search_form();?>
+    <?php wp_link_pages();?>
 </div>
 </section>
 
